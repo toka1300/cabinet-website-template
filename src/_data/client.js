c918cdd8131fe3149310e90239dda8c3f42fdc76
toka1300 <caseytokarchuk@gmail.com>
2024-09-19 08:33:13 -0400

    Updated content, branding, etc.

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,20 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Casey's Web Designs",
+    "email": "caseytokarchuk@gmail.com",
+    "phoneForTel": "613-857-9828",
+    "phoneFormatted": "(613) 857-9828",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "lineOne": "2391 Fitzroy Street",
+        "city": "Ottawa",
+        "state": "ON",
+        "postalCode": "K0A 1X0",
+        "country": "Canada",
+        "mapLink": "https://maps.app.goo.gl/VcGqWWJsG7Bgmmrt5"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.google.ca"
 };
